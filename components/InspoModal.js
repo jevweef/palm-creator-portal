31dba6da76d2ff998c5dac93b78f6e38a5bbc9ea
Evan Leith <jevanleith@Evans-MacBook-Pro.local>
2026-03-26 23:50:02 -0400

    Fix InspoModal syntax error: mismatched closing tags breaking Vercel build

--- components/InspoModal.js
+++ components/InspoModal.js
@@ -120,7 +120,8 @@ export default function InspoModal({ record, grade, onClose, onPrev, onNext, has
               <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
               </svg>
-          </button>
+            </button>
+          </div>
         </div>
 
         {/* Body */}
