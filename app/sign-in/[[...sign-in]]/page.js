f5316de7c66aa8b0338e1dd41401ac7253552a7e
Evan Leith <jevanleith@Evans-MacBook-Pro.local>
2026-04-03 13:37:37 -0400

    Set sign-in/sign-up page backgrounds to blush pink

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function SignInPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[#0a0a0a]">
+    <div className="min-h-screen flex items-center justify-center bg-[#FFECF7]">
       <SignIn />
     </div>
   )
